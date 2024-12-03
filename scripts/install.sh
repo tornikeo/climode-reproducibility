5eaac859c555529b05a03bdf4768f979049eb4fb
tornikeo <tornikeonoprishvili@gmail.com>
2024-12-03 10:18:15 +0000

    Simplify install

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,10 +13,18 @@ sudo apt-get install tree -y
 sudo apt-get install unzip -y 
 sudo apt-get install build-essential -y
 
+## Pytorch geom stuff
 conda install pytorch-cluster -c pyg -c pytorch -c nvidia -y
 
+# Numba
 conda install -c conda-forge cuda-nvcc cuda-nvrtc "cuda-version>=12.0" -y
 conda install numba -y
+pip install git+https://github.com/patrick-kidger/torchcubicspline.git
+
+# weather prediction items
 conda install -c conda-forge xarray dask netCDF4 bottleneck -y
 
-pip install pooch matplotlib numpy ipykernel tqdm pandas datasets transformers
+# python utilities
+pip install pooch matplotlib numpy ipykernel \
+    tqdm pandas datasets transformers \
+    scipy properscoring torchdiffeq
