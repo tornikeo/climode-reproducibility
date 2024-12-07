1e1d6067c9b011fca554f287e86d5c57c75ffa1d
tornikeo <tornikeonoprishvili@gmail.com>
2024-12-07 20:22:03 +0000

    Small changes to installs

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,10 +3,12 @@
 # most of the heaviest packages are already available on
 # docker pytorch devel
 # You can use for example: 
-# docker pull pytorch/pytorch:2.4.1-cuda12.4-cudnn9-devel
+# docker pull pytorch/pytorch:2.2.1-cuda12.1-cudnn8-devel
 # Which has most of what you will need already installed.
 
 set -e
+
+python -c "import torch; assert torch.cuda.is_available()"
 
 ## General utils
 sudo apt-get install tree -y
@@ -14,7 +16,7 @@ sudo apt-get install unzip -y
 sudo apt-get install build-essential -y
 
 ## Pytorch geom stuff
-conda install pytorch-cluster -c pyg -c pytorch -c nvidia -y
+conda install pyg -c pyg
 
 # Numba
 conda install -c conda-forge cuda-nvcc cuda-nvrtc "cuda-version>=12.0" -y
@@ -28,3 +30,7 @@ conda install -c conda-forge xarray dask netCDF4 bottleneck -y
 pip install pooch matplotlib numpy ipykernel \
     tqdm pandas datasets transformers \
     scipy properscoring torchdiffeq
+
+
+code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
